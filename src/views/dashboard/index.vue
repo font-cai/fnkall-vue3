125c5c2beca23bca3fb5c0a9d7bb3f077e90b7e0
admiredemo <jiaozhufensi@qq.com>
2025-04-13 12:51:12 +0800

    init

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="p-5">
+    <el-card shadow="never">
+      <el-row justify="space-between">
+        <el-col :span="18" :xs="24">
+          <div class="flex h-full items-center">
+            <img
+              class="w-20 h-20 mr-5 rounded-full"
+              :src="userStore.userInfo.avatar + '?imageView2/1/w/80/h/80'"
+            />
+            <div>
+              <p>{{ greetings }}</p>
+              <p class="text-sm text-gray">今日天气晴朗，气温在15℃至25℃之间，东南风。</p>
+            </div>
+          </div>
+        </el-col>
+      </el-row>
+    </el-card>
+
+    <el-row class="mt-3">
+      <el-col>
+        <el-card>
+          <el-descriptions :column="1" border>
+            <el-descriptions-item label="项目名称">
+              <span class="font-bold">全干之路</span>
+            </el-descriptions-item>
+            <el-descriptions-item label="项目描述">🚀 施工中....</el-descriptions-item>
+          </el-descriptions>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: "Dashboard",
+  inheritAttrs: false,
+});
+import { useUserStore } from "@/store/modules/user.store";
+
+const userStore = useUserStore();
+const date: Date = new Date();
+const greetings = computed(() => {
+  const hours = date.getHours();
+  if (hours >= 6 && hours < 8) {
+    return "晨起披衣出草堂，轩窗已自喜微凉🌅！";
+  } else if (hours >= 8 && hours < 12) {
+    return "上午好，" + userStore.userInfo.nickname + "！";
+  } else if (hours >= 12 && hours < 18) {
+    return "下午好，" + userStore.userInfo.nickname + "！";
+  } else if (hours >= 18 && hours < 24) {
+    return "晚上好，" + userStore.userInfo.nickname + "！";
+  } else {
+    return "偷偷向银河要了一把碎星，只等你闭上眼睛撒入你的梦中，晚安🌛！";
+  }
+});
+</script>
+
+<style lang="scss" scoped></style>
